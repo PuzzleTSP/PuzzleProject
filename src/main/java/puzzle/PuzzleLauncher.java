42d473286ff72c329454bf16c788be2d91991386
Dawson McKenzie <dhmckenz@mtu.edu>
2018-10-19 13:39:32 -0400

    Replaced BuggyCode.java and BuggyCodeTest.java with PuzzleLauncher.java and PuzzleLauncherTest.java

---
+++ src/main/java/puzzle/PuzzleLauncher.java
@@ -0,0 +1,15 @@
+// @author dhmckenz
+
+package puzzle;
+
+import java.util.*;
+
+public class PuzzleLauncher {
+
+  private boolean launchSuccessful = false;
+
+  public boolean launch() {
+    launchSuccessful = true;
+    return launchSuccessful;
+  }
+}
---
+++ src/test/java/PuzzleLauncherTest.java
@@ -0,0 +1,21 @@
+import puzzle.*;
+
+import java.util.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+
+public class PuzzleLauncherTest {
+
+  private PuzzleLauncher p = null;
+
+  @Before
+  public void setup() {
+    p = new PuzzleLauncher();
+  }
+
+  @Test
+  public void sampleTest() {
+    assertTrue(p.launch());
+  }
+}
