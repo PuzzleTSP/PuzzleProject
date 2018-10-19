24113ad37bf8acde2de9141c6cc1509e78827ebe
Jacob Vanthof <jdvantho@c28-0117-03.ad.mtu.edu>
2018-10-19 12:47:25 -0400

    Changed project structure for build file.

---
+++ src/test/java/BuggyCodeTest.java
@@ -0,0 +1,16 @@
+import buggy.*;
+
+import java.util.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class BuggyCodeTest {
+
+  @Test
+  public void sampleTest() {
+    // Add testing logic here
+  }
+
+  // Add more tests as necessary
+
+}
