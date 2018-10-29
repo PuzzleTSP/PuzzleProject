8e612e8d716b93a36e1e892c391aac7559d85779
Giulio Vario <gtvario@yahoo.com>
2018-10-29 12:47:59 -0400

    Added PuzzleType2 to work with different layouts

---
+++ src/main/java/puzzle/PuzzleType2.java
@@ -0,0 +1,25 @@
+package puzzle;
+
+import javafx.scene.layout.Pane;
+
+public class PuzzleType2 implements Puzzle {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pane run() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void exit() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
