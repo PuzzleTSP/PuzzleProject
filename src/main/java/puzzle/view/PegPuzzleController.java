230aa1ca4bc730c373f3468023539f29f38a4f2d
Giulio <gtvario@yahoo.com>
2018-11-08 20:41:55 -0500

    Added peg puzzle graphics and started the controller

--- src/main/java/puzzle/view/PegPuzzleController.java
+++ src/main/java/puzzle/view/PegPuzzleController.java
@@ -1,5 +1,79 @@
 package puzzle.view;
 
-public class PegPuzzleController {
+import java.util.ArrayList;
 
+import javafx.fxml.FXML;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import puzzle.PuzzleLauncher;
+
+public class PegPuzzleController {
+	private PuzzleLauncher app;
+	
+	@FXML
+	private Circle peg1;
+	@FXML
+	private Circle peg2;
+	@FXML
+	private Circle peg3;
+	@FXML
+	private Circle peg4;
+	@FXML
+	private Circle peg5;
+	@FXML
+	private Circle peg6;
+	@FXML
+	private Circle peg7;
+	@FXML
+	private Circle peg8;
+	@FXML
+	private Circle peg9;
+	@FXML
+	private Circle peg10;
+	@FXML
+	private Circle peg11;
+	@FXML
+	private Circle peg12;
+	@FXML
+	private Circle peg13;
+	@FXML
+	private Circle peg14;
+	@FXML
+	private Circle peg15;
+	
+	private ArrayList<Circle> pegs = new ArrayList<Circle>();
+	
+	@FXML
+	private void initialize() {	
+		if(pegs.isEmpty()) {
+			pegs.add(peg1);
+			pegs.add(peg2);
+			pegs.add(peg3);
+			pegs.add(peg4);
+			pegs.add(peg5);
+			pegs.add(peg6);
+			pegs.add(peg7);
+			pegs.add(peg8);
+			pegs.add(peg9);
+			pegs.add(peg10);
+			pegs.add(peg11);
+			pegs.add(peg12);
+			pegs.add(peg13);
+			pegs.add(peg14);
+			pegs.add(peg15);
+		}
+		
+		int startPeg = (int) (Math.random() * 14);
+		
+		pegs.get(startPeg).setFill(Color.WHITE);
+	}
+	
+	@FXML
+	private void closeApp() {
+		app.getPrimaryStage().close();
+	}
+	
+	public void setApp(PuzzleLauncher app) {
+		this.app = app;
+	}
 }
