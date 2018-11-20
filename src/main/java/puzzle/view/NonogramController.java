7613a86320bb4ba44e2674cffc54bf936930145a
Dawson McKenzie <dhmckenz@mtu.edu>
2018-11-19 23:41:49 -0500

    Updates to nonogram, added labels

--- src/main/java/puzzle/view/NonogramController.java
+++ src/main/java/puzzle/view/NonogramController.java
@@ -1,9 +1,17 @@
 package puzzle.view;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
 import puzzle.PuzzleLauncher;
 import puzzle.model.nonogram.Square;
@@ -12,20 +20,54 @@ public class NonogramController {
 	
 	private PuzzleLauncher app;
 	private Square[][] squares;
+	private ObservableList<Node> gridChildren;
+	private String[][] colLabels;
+	private String[][] rowLabels;
 	
-	@FXML private Label row1; @FXML private Label row2; @FXML private Label row3; @FXML private Label row4; @FXML private Label row5;
-	@FXML private Label row6; @FXML private Label row7; @FXML private Label row8; @FXML private Label row9; @FXML private Label row10;
-	@FXML private Label col1; @FXML private Label col2; @FXML private Label col3; @FXML private Label col4; @FXML private Label col5;
-	@FXML private Label col6; @FXML private Label col7; @FXML private Label col8; @FXML private Label col9; @FXML private Label col10;
+	@FXML
+	GridPane grid;
 	
 	@FXML
 	private void initialize() {
+		gridChildren = grid.getChildren();
+		colLabels = new String[10][5];
+		rowLabels = new String[10][5];
 		squares = new Square[10][10];
+		
+		// Init Squares
 		for (int i = 0; i <= 9; i++) {
 			for (int j = 0; j <= 9; j++) {
 				squares[i][j] = new Square();
 			}
 		}
+
+		loadNonogram("cat.txt");
+		
+		// check that children are correct
+//		for (int i = 0; i <= 10; i++) {
+//			for (int j = 0; j <= 10; j++) {
+//				Node child = gridChildren.get(11*i+j);
+//				System.out.println(GridPane.getRowIndex(child) + " " + GridPane.getColumnIndex(child));
+//			}
+//			System.out.println();
+//		}
+		
+		for (int i = 0; i < 10; i++) {
+			VBox vBox = (VBox) gridChildren.get(i+1);
+			ObservableList<Node> vBoxChildren = vBox.getChildren();
+			
+			HBox hBox = (HBox) gridChildren.get(11*(i+1));
+			ObservableList<Node> hBoxChildren = hBox.getChildren();
+			
+			for (int j = 0; j < 5; j++) {
+				Label colLabel = (Label) vBoxChildren.get(j);
+				colLabel.setText(colLabels[i][j]);
+				
+				Label rowLabel = (Label) hBoxChildren.get(j);
+				rowLabel.setText(rowLabels[i][j]);
+			}
+		}
+		
 	}
 	
 	@FXML
@@ -39,4 +81,32 @@ public class NonogramController {
 		this.app = app;
 	}
 
+	private void loadNonogram(String fileName) {
+		File modelNonogramDir = new File("./src/main/java/puzzle/model/nonogram");
+		try (Scanner in = new Scanner(new File(modelNonogramDir, fileName))) {
+			// Load column labels
+			for (int i = 0; i < 10; i++) {
+				for (int j = 0; j < 5; j++) {
+					colLabels[i][4-j] = setLabelText(in.nextInt());
+				}
+			}
+			// Load row labels
+			for (int i = 0; i < 10; i++) {
+				for (int j = 0; j < 5; j++) {
+					rowLabels[i][4-j] = setLabelText(in.nextInt());
+				}
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private String setLabelText(int num) {
+		String labelText = " ";
+		if (num > 0) {
+			labelText = String.valueOf(num);
+		}
+		return labelText;
+	}
 }
