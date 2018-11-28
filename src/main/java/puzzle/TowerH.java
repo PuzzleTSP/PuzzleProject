0ddb057ce3e81abc4d7b85bde30a1676896a15a3
Jacob-Vanthof <38826597+Jacob-Vanthof@users.noreply.github.com>
2018-11-28 06:25:17 -0500

    Create TowerH.java
    Created start of Towers of Hanoi

---
+++ src/main/java/puzzle/TowerH.java
@@ -0,0 +1,20 @@
+package puzzle;
+
+import java.util.Stack;
+
+public class TowerH {
+	private Stack<Integer> Tower1 = new Stack<Integer>();
+	private Stack<Integer> Tower2 = new Stack<Integer>();
+	private Stack<Integer> Tower3 = new Stack<Integer>();
+	
+	
+	/**
+	 * Boolean to tell whether a move is acceptable.
+	 */
+	protected boolean acceptableMove(Stack<Integer> from, Stack<Integer> to) {
+		if (from.peek() < to.peek() ) return false;
+		else return true;
+	}
+	
+	
+}
