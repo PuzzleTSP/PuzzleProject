78c71427771ef2b82a4accbe730aef54d2d55676
Dawson McKenzie <dhmckenz@mtu.edu>
2018-12-05 12:53:31 -0500

    Fixed testing for nonogram

--- src/test/java/TestSquareState.java
+++ src/test/java/TestSquareState.java
@@ -8,21 +8,18 @@ public class TestSquareState {
 	@Test
 	public void testXOUT() {
 		assertEquals(-1, SquareState.XOUT.getValue());
-		assertEquals(Color.WHITE, SquareState.XOUT.getColor());
 		assertTrue(SquareState.XOUT.getxVisible());
 	}
 	
 	@Test
 	public void testEMPTY() {
 		assertEquals(0, SquareState.EMPTY.getValue());
-		assertEquals(Color.WHITE, SquareState.EMPTY.getColor());
 		assertFalse(SquareState.EMPTY.getxVisible());
 	}
 	
 	@Test
 	public void testFULL() {
 		assertEquals(1, SquareState.FULL.getValue());
-		assertEquals(Color.BLACK, SquareState.FULL.getColor());
 		assertFalse(SquareState.FULL.getxVisible());
 	}
 }
